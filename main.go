c942740995e0e8470646524de03805a098c1a28a
Jose Leon <leonj1@gmail.com>
2021-04-03 14:30:12 -0400

    FIX trying to fix DSN to open sqlite file

--- main.go
+++ main.go
@@ -56,8 +56,7 @@ func main() {
 		Dir:     "migrations",
 	}
 
-	driver, err := sqlite.New(fmt.Sprintf("file::%s:?cache=shared&_busy_timeout=50000", sqliteDatabase), true)
-
+	driver, err := sqlite.New(fmt.Sprintf("file:%s", sqliteDatabase), true)
 	if err != nil {
 		log.Printf("Problem connecting to db: %s", err.Error())
 		os.Exit(1)
